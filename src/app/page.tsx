259cd34d6269ea7a612cdcfba9bedd6a8b342b04
RanakMedlr <ranak.shah@medlr.in>
2024-07-03 05:41:55 +0530

    first setup

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-slate-50 grainy-light">
+      <section>
+        <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
+          <div className='col-span-2 px-6 lg:px-0 lg:pt-4'>
+            <div className='relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start'>
+            <div className='absolute w-28 left-0 -top-20 hidden lg:block'>
+                <div className='absolute inset-x-0 bottom-0 bg-gradient-to-t via-slate-50/50 from-slate-50 h-28' />
+                <img src='/snake-1.png' className='w-full' />
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    </div>
+  );
+}
