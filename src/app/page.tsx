79ec9399cf63d25947d9f30d589f7b5c8ea043d0
RanakMedlr <ranak.shah@medlr.in>
2024-07-05 05:50:20 +0530

    home page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,28 @@ import { FlipWords } from '@/components/ui/flip-words';
 import Footer from '@/components/footer';
 import { Button } from "@/components/ui/button";
 import { useRouter } from 'next/navigation';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 const Home: React.FC = () => {
   const router = useRouter();
   const words = ["better", "cute", "beautiful", "modern"];
   const [isBlackBackground, setIsBlackBackground] = useState(false);
+
+  const controls1 = useAnimation();
+  const controls2 = useAnimation();
+  
+  const { ref: ref1, inView: inView1 } = useInView();
+  const { ref: ref2, inView: inView2 } = useInView();
+
+  useEffect(() => {
+    if (inView1) {
+      controls1.start({ x: 0, opacity: 1 });
+    }
+    if (inView2) {
+      controls2.start({ x: 0, opacity: 1 });
+    }
+  }, [controls1, controls2, inView1, inView2]);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -41,7 +58,7 @@ const Home: React.FC = () => {
 
   return (
     <div className={`text-3xl transition-colors duration-500 ${isBlackBackground ? 'bg-black text-white' : ''}`}>
-      <div className="relative w-full h-[90vh] pt-16"> {/* Adjusted padding */}
+      <div className="relative w-full h-[90vh] pt-16">
         <div id="background-image" className="absolute top-0 left-0 w-full h-full overflow-hidden transition-transform duration-500 ease-out">
           <Image src={hero} alt="Hero" layout="fill" objectFit="cover" />
           <div className="h-[40rem] flex justify-center items-center mr-[100vh]">
@@ -53,6 +70,34 @@ const Home: React.FC = () => {
       <div className="h-[70vh] text-9xl flex items-center">
         <h1>Solid It UP</h1>
       </div>
+
+      <div className="h-[70vh] text-9xl flex items-center">
+        <h1>Have Some cool design in Mind?</h1>
+      </div>
+      
+      <motion.div
+        ref={ref1}
+        initial={{ x: '80vw', opacity: 0.5 }}
+        animate={controls1}
+        transition={{ type: 'spring', stiffness: 70 }}
+        className="text-7xl text-right mb-24 flex justify-end  gap-10"
+      >
+        Let AI fix it <Button className='text-4xl w-48 h-16 mr-40 ml-10 hover:bg-slate-700'>Go</Button>
+      </motion.div>
+
+      <div className="h-[70vh] text-9xl flex items-center">
+        <h1>OR a nice picture...</h1>
+      </div> 
+      
+      <motion.div
+        ref={ref2}
+        initial={{ x: '80vw', opacity: 0 }}
+        animate={controls2}
+        transition={{ type: 'spring', stiffness: 70 }}
+        className="text-7xl text-right mx-52 mb-24 flex justify-end"
+      >
+        Make your own one <Button className='text-4xl w-48 h-16  ml-10 p-9 hover:bg-slate-700'>Go</Button>
+      </motion.div>
 
       <ParallaxScroll />
 
--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -36,9 +36,9 @@ const sheetVariants = cva(
         bottom:
           "inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom",
         left:
-          "inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm",
+          "inset-y-0 left-0 h-full border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left",
         right:
-          "inset-y-0 right-0 h-full w-3/4 border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm",
+          "inset-y-0 right-0 h-full border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right",
       },
     },
     defaultVariants: {
@@ -50,23 +50,24 @@ const sheetVariants = cva(
 interface SheetContentProps
   extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
     VariantProps<typeof sheetVariants> {
-  side?: "top" | "bottom" | "left" | "right"; // Add the side prop
+  side?: "top" | "bottom" | "left" | "right";
+  width?: string;
 }
 
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
   SheetContentProps
->(({ side = "right", className, children, ...props }, ref) => (
+>(({ side = "right", width = "w-3/4 sm:max-w-sm", className, children, ...props }, ref) => (
   <SheetPortal>
     <SheetOverlay />
     <SheetPrimitive.Content
       ref={ref}
-      className={cn(sheetVariants({ side }), className)}
+      className={cn(sheetVariants({ side }), width, className)}
       {...props}
     >
       {children}
-      <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-        <Cross2Icon className="h-4 w-4" />
+      <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70  transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring disabled:pointer-events-none data-[state=open]:bg-secondary">
+        <Cross2Icon className="h-4 w-4 text-white" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
     </SheetPrimitive.Content>
